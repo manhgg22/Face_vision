6e9596fa8fd772daca90c0d0b5c405dc0e2e41c7
manhgg22 <manhgg22@gmail.com>
2026-04-23 18:10:06 +0700

    UI: Add X button and improve preview experience

--- start_face_service.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo ============================================================
-echo   Starting Face Analysis Service (DeepFace - Port 8001)
-echo ============================================================
-cd /d "%~dp0"
-python face_service.py
-pause
