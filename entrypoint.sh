bcacc3ac9ceff9ee6c4d6be0d19bf5c0a791d00b
manhgg22 <manhgg22@gmail.com>
2026-04-24 09:48:32 +0700

    chore: optimize project structure for Docker, fix entrypoint and remove unused metadata files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,4 @@
-# Example content
-echo "Starting the application..."
-exec "$@"
-
-gunicorn --workers=1 --timeout=7200 --bind=0.0.0.0:5000 --log-level=debug --access-logformat='%(h)s - - [%(t)s] "%(r)s" %(s)s %(b)s %(L)s' --access-logfile=- "app:create_app()"
+#!/bin/sh
+echo "🚀 Starting FaceID Pro Service in Docker..."
+# Use python to run the service script directly
+python face_service.py
